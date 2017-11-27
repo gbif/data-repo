c0135ec994f0057b20e5c922070c6f2bf25d19d2
Federico Mendez <fmendez@gbif.org>
2017-11-27 16:20:29 +0100

    validating that when oneof  identifier or identifierScheme are specified, both must be specified

--- data-repo-server-fs/src/main/java/org/gbif/datarepo/api/validation/constraints/IdentifierSchemeValidator.java
+++ data-repo-server-fs/src/main/java/org/gbif/datarepo/api/validation/constraints/IdentifierSchemeValidator.java
@@ -11,7 +11,9 @@ import javax.validation.ConstraintValidatorContext;
  */
 public class IdentifierSchemeValidator implements ConstraintValidator<ValidIdentifierScheme,Creator> {
 
-  private static final String DEFAULT_ERROR_MSG = "The identifier  %S is invalid for the identifier scheme %s";
+  private static final String INVALID_ERROR_MSG = "The identifier %s is invalid for the identifier scheme %s";
+
+  private static final String ID_AND_SCHEME_REQUIRED_MSG = "The identifier and identifier scheme must be set of one of them is present";
 
   @Override
   public void initialize(ValidIdentifierScheme constraintAnnotation) {
@@ -21,10 +23,16 @@ public class IdentifierSchemeValidator implements ConstraintValidator<ValidIdent
   @Override
   public boolean isValid(Creator value, ConstraintValidatorContext context) {
     boolean result = true;
+    //if identifier or identifierScheme are specified, both must be present
+    if ((value.getIdentifier() != null && value.getIdentifierScheme() == null) ||
+        (value.getIdentifier() == null && value.getIdentifierScheme() != null)) {
+      context.buildConstraintViolationWithTemplate(ID_AND_SCHEME_REQUIRED_MSG).addConstraintViolation();
+      result= false;
+    }
     if (value.getIdentifier() != null && value.getIdentifierScheme() != null) {
       if(!IdentifierSchemaValidatorFactory.getValidator(value.getIdentifierScheme())
         .isValid(value.getIdentifier())) {
-        context.buildConstraintViolationWithTemplate(String.format(DEFAULT_ERROR_MSG, value.getIdentifier(),
+        context.buildConstraintViolationWithTemplate(String.format(INVALID_ERROR_MSG, value.getIdentifier(),
                                                                    value.getIdentifierScheme()))
         .addConstraintViolation();
         result = false;
